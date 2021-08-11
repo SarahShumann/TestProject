5c7899c5f4c73bd7fc25071b5167a72600afcb33
Sarah Shuman <49661770+SarahShumann@users.noreply.github.com>
2021-08-11 22:36:33 +0300

    Update JSFILE.js

--- JSFILE.js
+++ JSFILE.js
@@ -1,5 +1,5 @@
-var Player1Score = 0;
-var computerScore = 0;
+var Player1Score = 0; //player's score
+var computerScore = 0; //computer's score
 
 document.getElementById('rock').onclick = Rock;
 document.getElementById('paper').onclick = Paper;
@@ -58,6 +58,7 @@ function play(Player1) {
   
 }
 
+//a fucntion to randomly choose between the 3 choices.
 function getComputerPlayer() {
   var plays = ['rock', 'paper', 'scissors'];
   var play = plays[Math.floor(Math.random() * plays.length)];

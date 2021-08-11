8d428604c8cbce9880ff6c5d7f5e320616cf6e83
Sarah Shuman <49661770+SarahShumann@users.noreply.github.com>
2021-08-11 22:34:20 +0300

    Create JSFILE.js

---
+++ JSFILE.js
@@ -0,0 +1,65 @@
+var Player1Score = 0;
+var computerScore = 0;
+
+document.getElementById('rock').onclick = Rock;
+document.getElementById('paper').onclick = Paper;
+document.getElementById('scissors').onclick = Scissors;
+
+function Rock() {
+  play("rock");
+}
+function Paper() {
+  play("paper");
+}
+function Scissors() {
+  play("scissors");
+}
+
+function play(Player1) {
+  
+  computerPlayer = getComputerPlayer();
+  
+  document.getElementById('status').innerHTML = "<p>You played <strong>" + Player1 + "</strong>. The computer played <strong>" + computerPlayer + "</strong>.</p>";
+  
+  if(Player1 == 'rock') {
+    if(computerPlayer == 'rock') {
+      document.getElementById('status').innerHTML += "<p>It's a Draw </p>";
+    } else if (computerPlayer == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You lose </p>";
+      computerScore++;
+    } else if (computerPlayer == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You win </p>";
+      Player1Score++;
+    }
+  } else if (Player1 == 'paper') {
+    if(computerPlayer == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You win </p>";
+      Player1Score++;
+    } else if (computerPlayer == 'paper') {
+      document.getElementById('status').innerHTML += "<p>It's a Draw </p>";
+    } else if (computerPlayer == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>You lose </p>";
+      computerScore++;
+    }  
+  } else if (Player1 == 'scissors') {
+    if(computerPlayer == 'rock') {
+      document.getElementById('status').innerHTML += "<p>You lose </p>";
+      computerScore++;
+    } else if (computerPlayer == 'paper') {
+      document.getElementById('status').innerHTML += "<p>You win </p>";
+      Player1Score++;
+    } else if (computerPlayer == 'scissors') {
+      document.getElementById('status').innerHTML += "<p>It's a Draw </p>";
+    }  
+  }
+  
+  document.getElementById('Player1Score').innerHTML = Player1Score;
+  document.getElementById('computerScore').innerHTML = computerScore;
+  
+}
+
+function getComputerPlayer() {
+  var plays = ['rock', 'paper', 'scissors'];
+  var play = plays[Math.floor(Math.random() * plays.length)];
+  return play;
+}
